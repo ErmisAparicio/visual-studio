ddeff7a78ffc7fa15d9003a89cca63767793917a
ErmisAparicio <ermis.6@gmail.com>
2014-07-09 15:49:38 -0600

    delete

--- Visual studio/PM/Principal.Window/Properties/Settings.Designer.cs
+++ Visual studio/PM/Principal.Window/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Principal.Window.Properties {
+namespace Principal.Mayer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
